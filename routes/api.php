ce9281abd87d88be63db083a99758b29afd8d509
unknown <admin@JMJGN-96.dcjmjain.com>
2025-01-03 18:19:44 +0530

    backend file upload

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,24 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\BrandController;
+use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::post('/admin/login',[AuthController::class,'authenticate']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    // Route::get('categories',[CategoryController::class,'index']);
+    // Route::get('categories/{id}',[CategoryController::class,'show']);
+    // Route::put('categories/{id}',[CategoryController::class,'update']);
+    // Route::delete('categories/{id}',[CategoryController::class,'destroy']);
+    // Route::post('categories',[CategoryController::class,'store']);
+
+    Route::resource('categories',CategoryController::class);
+    Route::resource('brands',BrandController::class);
 });

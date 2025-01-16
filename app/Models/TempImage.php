5893cc24e07e9b047cf485f17b7e0358d9656c7f
unknown <admin@JMJGN-96.dcjmjain.com>
2025-01-16 18:20:54 +0530

    product image upload done

--- app/Models/TempImage.php
+++ app/Models/TempImage.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class TempImage extends Model
 {
-    use HasFactory;
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute() {
+        if ($this->name == "") {
+            return "";
+        }
+
+        return asset('/uploads/temp/thumb/'.$this->name);
+    }
 }
